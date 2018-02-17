34bd37a99dddb6623cf8a32dea44bc973d2c3205
Alexander Patrick <adp90@case.edu>
2018-02-17 13:15:25 -0500

    Added React.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @response = HTTParty.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT&apikey=demo')
   end
 end
